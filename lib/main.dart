a406050b16692a590a70c0c42536551252c78d9e
kiyosuke <k.kiyosuke1999@gmail.com>
2022-11-14 22:16:01 +0900

    Remove URL `#`

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:hotel_in_aomori/src/routing/app_router.dart';
 
 void main() {
+  setUrlStrategy(PathUrlStrategy());
   runApp(const ProviderScope(child: MyApp()));
 }
 
